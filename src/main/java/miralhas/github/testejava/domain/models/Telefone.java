d04328ba5c963d2f8e556f76805c9c843dd0a1b9
Miralhas <victor2.miralhas@gmail.com>
2025-09-04 15:20:09 -0300

    feat: create model telefone

---
+++ src/main/java/miralhas/github/testejava/domain/models/Telefone.java
@@ -0,0 +1,48 @@
+package miralhas.github.testejava.domain.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import miralhas.github.testejava.domain.models.enums.TelefoneTipo;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Telefone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long id;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TelefoneTipo tipo;
+
+	@Column(nullable = false)
+	private String ddd;
+
+	@Column(nullable = false)
+	private String numero;
+
+
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+		Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+		if (thisEffectiveClass != oEffectiveClass) return false;
+		Telefone telefone = (Telefone) o;
+		return getId() != null && Objects.equals(getId(), telefone.getId());
+	}
+
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+	}
+}
---
+++ src/main/java/miralhas/github/testejava/domain/models/enums/TelefoneTipo.java
@@ -0,0 +1,5 @@
+package miralhas.github.testejava.domain.models.enums;
+
+public enum TelefoneTipo {
+	CELULAR, RESIDENCIAL, COMERCIAL
+}
