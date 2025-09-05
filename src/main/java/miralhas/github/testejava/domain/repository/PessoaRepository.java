a9f10936ae109ae458d0a47de55cc3eebc142a17
Miralhas <victor2.miralhas@gmail.com>
2025-09-05 17:45:59 -0300

    feat: add save and remove methods on PessoaRepository

--- src/main/java/miralhas/github/testejava/domain/repository/PessoaRepository.java
+++ src/main/java/miralhas/github/testejava/domain/repository/PessoaRepository.java
@@ -1,9 +1,41 @@
 package miralhas.github.testejava.domain.repository;
 
-import jakarta.data.repository.CrudRepository;
-import jakarta.data.repository.Repository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import miralhas.github.testejava.domain.models.Pessoa;
 
-@Repository
-public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@ApplicationScoped
+public class PessoaRepository implements CrudRepository<Pessoa, Long> {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public List<Pessoa> findAll() {
+		return em.createQuery("SELECT p from Pessoa p", Pessoa.class).getResultList();
+	}
+
+	@Override
+	public Optional<Pessoa> findById() {
+		return Optional.empty();
+	}
+
+	@Override
+	public void save(Pessoa p) {
+		if (Objects.isNull(p.getId())) {
+			em.persist(p);
+			return;
+		}
+		em.merge(p);
+	}
+
+	@Override
+	public void delete(Pessoa pessoa) {
+		em.remove(em.contains(pessoa) ? pessoa : em.merge(pessoa));
+	}
 }
